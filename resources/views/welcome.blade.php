dfbec21dd5c2606ee93ce1aef48d2e2080b2e4b5
claudio <claudiopero@estudiopero.com>
2020-08-23 21:33:21 +0100

    2 commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,100 +1,105 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-        <title>Laravel</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <title>YouTube</title>
+  </head>
+  <body>
+<?php
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
 
-            .full-height {
-                height: 100vh;
-            }
+//require_once '../../vendor/autoload.php';
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+$htmlBody = <<<END
+<form method="GET">
+  <div>
+    Search Term: <input type="search" id="q" name="q" placeholder="Enter Search Term">
+  </div>
+  <div>
+    Max Results: <input type="number" id="maxResults" name="maxResults" min="1" max="50" step="1" value="25">
+  </div>
+  <input type="submit" value="Search">
+</form>
+END;
 
-            .position-ref {
-                position: relative;
-            }
+// This code will execute if the user entered a search query in the form
+// and submitted the form. Otherwise, the page displays the form above.
+if (isset($_GET['q']) && isset($_GET['maxResults'])) {
+  /*
+   * Set $DEVELOPER_KEY to the "API key" value from the "Access" tab of the
+   * Google API Console <https://console.developers.google.com/>
+   * Please ensure that you have enabled the YouTube Data API for your project.
+   */
+  $DEVELOPER_KEY = 'AIzaSyC9yRV-PnaFesxj-IjTfTf60E1bAPDAw00';
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+  $client = new Google_Client();
+  $client->setDeveloperKey($DEVELOPER_KEY);
 
-            .content {
-                text-align: center;
-            }
+  // Define an object that will be used to make all API requests.
+  $youtube = new Google_Service_YouTube($client);
 
-            .title {
-                font-size: 84px;
-            }
+  $htmlBody = '';
+  try {
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
+    // Call the search.list method to retrieve results matching the specified
+    // query term.
+    $searchResponse = $youtube->search->listSearch('id,snippet', array(
+      'q' => $_GET['q'],
+      'maxResults' => $_GET['maxResults'],
+    ));
 
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+    $videos = '';
+    $channels = '';
+    $playlists = '';
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
+    // Add each result to the appropriate list, and then display the lists of
+    // matching videos, channels, and playlists.
+    foreach ($searchResponse['items'] as $searchResult) {
+      switch ($searchResult['id']['kind']) {
+        case 'youtube#video':
+          $videos .= sprintf('<li>%s (%s)</li>',
+              $searchResult['snippet']['title'], $searchResult['id']['videoId']);
+          break;
+        case 'youtube#channel':
+          $channels .= sprintf('<li>%s (%s)</li>',
+              $searchResult['snippet']['title'], $searchResult['id']['channelId']);
+          break;
+        case 'youtube#playlist':
+          $playlists .= sprintf('<li>%s (%s)</li>',
+              $searchResult['snippet']['title'], $searchResult['id']['playlistId']);
+          break;
+      }
+    }
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+    $htmlBody .= <<<END
+    <h3>Videos</h3>
+    <ul>$videos</ul>
+    <h3>Channels</h3>
+    <ul>$channels</ul>
+    <h3>Playlists</h3>
+    <ul>$playlists</ul>
+END;
+  } catch (Google_Service_Exception $e) {
+    $htmlBody .= sprintf('<p>A service error occurred: <code>%s</code></p>',
+      htmlspecialchars($e->getMessage()));
+  } catch (Google_Exception $e) {
+    $htmlBody .= sprintf('<p>An client error occurred: <code>%s</code></p>',
+      htmlspecialchars($e->getMessage()));
+  }
+}
+?>
+ <?=$htmlBody?>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
-    </body>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+  </body>
 </html>
